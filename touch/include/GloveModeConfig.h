d1748eea54b74e8c93aa7cf0a5156e5100399540
genoxci-dev <genoxci.dev@gmail.com>
2026-03-15 04:24:24 +0800

    sm8650-common: Add support for glove mode

---
+++ touch/include/GloveModeConfig.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace touch {
+
+constexpr const char* kGloveModePath =
+    "/sys/devices/platform/soc/ac0000.qcom,qupv3_1_geni_se/a90000.spi/spi_master/spi0/spi0.0/synaptics_tcm_hbp.0/glove_mode";
+
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
